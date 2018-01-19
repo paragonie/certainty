fc4778349fec50737bdf87268f0ce71e38a0ded7
Paragon Initiative Enterprises, LLC <security@paragonie.com>
2018-01-19 17:10:53 -0500

    New CACert Bundle - 2018-01-17
    defe310a0184a12e4b1b3d147f1d77395dd7a09e3428373d019bef5d542ceba3  cacert.pem

--- local/signer.php
+++ local/signer.php
@@ -5,6 +5,10 @@ if (!\file_exists('keys.json')) {
     echo 'Keys don\'t exist!', PHP_EOL;
     exit(127);
 }
+if ($argc < 2) {
+    echo 'Usage: php signer.php /path/to/cacert.pem', PHP_EOL;
+    exit(1);
+}
 $file = \file_get_contents('keys.json');
 $json = \json_decode($file, true);
 
