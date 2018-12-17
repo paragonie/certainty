4945abff472458fe2d35865df91a1645bca1da47
Paragon Initiative Enterprises <security@paragonie.com>
2018-12-17 16:48:46 -0500

    Add unit test

--- test/ValidatorTest.php
+++ test/ValidatorTest.php
@@ -2,6 +2,8 @@
 namespace ParagonIE\Certainty\Tests;
 
 use ParagonIE\Certainty\Bundle;
+use ParagonIE\Certainty\Exception\CertaintyException;
+use ParagonIE\Certainty\RemoteFetch;
 use ParagonIE\Certainty\Validator;
 use PHPUnit\Framework\TestCase;
 
@@ -13,7 +15,13 @@ class ValidatorTest extends TestCase
 {
     /** @var Bundle $bundle */
     protected $bundle;
-    
+
+    /** @var string */
+    protected $dir;
+
+    /** @var string */
+    protected $dir2;
+
     /** @var Validator $validator */
     protected $validator;
 
@@ -28,6 +36,35 @@ class ValidatorTest extends TestCase
             '7b8eb84bbaa30c648f3fc9b28d720ab247314032cc4c1f8ad7bd13f7eb2a40a8',
             '456729f1ea34ea0712476e82a904664ead413157291ec47d7c1595795032f004cf6e5532cd8f80d54a8cb86e92dac71367677f110daba1cc2a1bbbcef4ef1a04'
         );
+        $this->dir = __DIR__ . '/static/data-valid';
+        if (!\is_dir($this->dir)) {
+            \mkdir($this->dir);
+        }
+        $this->dir2 = __DIR__ . '/static/data-valid2';
+        if (!\is_dir($this->dir2)) {
+            \mkdir($this->dir2);
+        }
+    }
+
+    /**
+     *
+     */
+    public function tearDown()
+    {
+        foreach ([$this->dir, $this->dir2] as $d) {
+            if (\file_exists($d . '/ca-certs.json')) {
+                \unlink($d . '/ca-certs.json');
+            }
+            if (\file_exists($d . '/ca-certs.cache')) {
+                \unlink($d . '/ca-certs.cache');
+            }
+            foreach (\glob($d . '/*.pem') as $f) {
+                $real = \realpath($f);
+                if (\strpos($real, $d) === 0) {
+                    \unlink($f);
+                }
+            }
+        }
     }
 
     /**
@@ -38,6 +75,33 @@ class ValidatorTest extends TestCase
         $this->assertTrue(
             $this->validator->checkSha256Sum($this->bundle),
             'Sha256sum of test case is wrong.'
+        );
+    }
+
+    /**
+     * @throws CertaintyException
+     * @throws \SodiumException
+     */
+    public function testChronicle()
+    {
+        if (\getenv('TRAVIS')) {
+            $this->markTestSkipped('Unknown GnuTLS errors are breaking TravisCI but the tests succeed locally.');
+        }
+        $remoteFetch = new RemoteFetch($this->dir);
+        $remoteFetch2 = new RemoteFetch($this->dir2);
+        $remoteFetch2->setChronicle(
+            'https://php-chronicle-replica.pie-hosted.com/chronicle/replica/_vi6Mgw6KXBSuOFUwYA2H2GEPLawUmjqFJbCCuqtHzGZ',
+            'MoavD16iqe9-QVhIy-ewD4DMp0QRH-drKfwhfeDAUG0='
+        );
+
+        $this->assertSame(
+            $remoteFetch->getLatestBundle()->getSha256Sum(),
+            $remoteFetch2->getLatestBundle()->getSha256Sum()
+        );
+
+        $this->assertSame(
+            $remoteFetch->getLatestBundle()->getChronicleHash(),
+            $remoteFetch2->getLatestBundle()->getChronicleHash()
         );
     }
 
