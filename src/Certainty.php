9b0bbc50456fdc1d903ca7fc3832fbe3097a66fd
Paragon Initiative Enterprises <security@paragonie.com>
2019-09-27 05:14:13 -0400

    Default to a 5 second cURL time-out

--- src/Certainty.php
+++ src/Certainty.php
@@ -36,6 +36,7 @@ class Certainty
             // https://github.com/curl/curl/blob/6aa86c493bd77b70d1f5018e102bc3094290d588/include/curl/curl.h#L1927
             $options['curl.options'][CURLOPT_SSLVERSION] = CURL_SSLVERSION_TLSv1_2 | (CURL_SSLVERSION_TLSv1 << 16);
         }
+        $options['connect_timeout'] = 5;
 
         return new Client($options);
     }
